258ad677a1322ed31ef2e7ab369e7ed18e182889
Mayor-Isaac <isaacogunyileka@gmail.com>
2026-04-06 14:18:57 +0100

    fix: Correct formatting of welcome message in Header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,13 +6,14 @@ export default function Header() {
   return (
     <div className="flex justify-between items-center py-3">
       <div className="flex flex-col gap-1">
-        <h1 className='font-medium text-[30px] '>
-          Welcome, <span>User</span>
+        <h1 className="font-medium text-[30px] ">
+          Welcome,{" "}
+          <span className="bg-linear-to-r from-yellow-700 via-green-500 to-emerald-400 bg-clip-text text-transparent">
+            User
+          </span>
         </h1>
         <span>{getCurrentDate()}</span>
       </div>
-
-      {/* <IoGrid size={25} className='' /> */}
     </div>
   );
 }
